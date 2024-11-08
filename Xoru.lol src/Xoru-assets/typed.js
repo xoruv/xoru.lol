dd8f996fc411bf742ebd25978aabf8863a7ec35d
xoru <165965570+xoruv@users.noreply.github.com>
2024-11-07 20:04:30 -0600

    Add files via upload

---
+++ Xoru.lol src/Xoru-assets/typed.js
@@ -0,0 +1,7 @@
+new Typed('#typed',{
+  strings : [,'quit Com','LIL SKID','connecting :O','Fuck Pedos','Sim Swapped :O','Public Enemy #1','https://raped.xyz/ :D'],
+  typeSpeed : 35,
+  delaySpeed : 90,
+  showCursor: false,
+  loop : true
+})
